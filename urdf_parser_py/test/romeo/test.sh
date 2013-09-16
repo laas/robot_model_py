4eae772ea6880a6f1d94bdc816290e53022e2d55
Thomas Moulard <thomas.moulard@gmail.com>
2013-09-16 19:15:17 +0900

    robot_model_py is now part of urdfdom.
    Remove all data but a warning.

--- urdf_parser_py/test/romeo/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-name=romeo
-orig="$(rospack find romeo_description)/urdf/$name.urdf"
-gen="/tmp/$name.urdf"
-patch="/tmp/$name.patch"
-
-rosrun urdf_parser_py display_urdf "$orig" -o "$gen"
-diff -u "$orig" "$gen" > "$patch"
